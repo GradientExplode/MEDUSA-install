101a6c9d9162b7dc1d059812797aa078e299bf3b
bobqianic <129547291+bobqianic@users.noreply.github.com>
2024-12-04 20:02:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 apt update
 
 # Install required packages
-apt install -y unzip build-essential cmake libboost-all-dev libhdf5-mpi-dev libmetis-dev libjpeg-dev libx11-dev
+apt install -y unzip build-essential cmake libboost-all-dev libhdf5-mpi-dev libmetis-dev libjpeg-dev libx11-dev libgsl-dev
 
 #To root
 cd /root
